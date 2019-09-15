44545a71a1071dc53faa72aaa440fab2ad6d0be0
nigh7sh4de <plotnikd@gmail.com>
2019-09-15 13:18:59 -0700

    init

---
+++ cafebot.py
@@ -0,0 +1,28 @@
+import tweepy
+from textblob import TextBlob
+
+# Twitter API Integration
+consumer_key = 'RrIenEvIdqeczejM2QJHYHC6C'
+consumer_secret = 'cLAKQFoNTiSyeH4dnKaAYIlQ8MLgWnukmBF5HSxK2dhYKmjnFM'
+
+access_token = '2490622578-9EHuAvx1pmLSJtL29RFdv8Tm81Sl8Ki5bmRLiUu'
+access_secret = 'gutWydSLUz4J1GvZ6EMfvs2XFhd59j3Pl9bBXwzqCiUtJ'
+
+# Tweepy Authorization Handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+# Create API variable
+api = tweepy.API(auth)
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+        analysis = TextBlob(status.text)
+        analysis_sentiment = analysis.sentiment
+        print(analysis_sentiment)
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+myStream.filter(track=['cafebot18'])
